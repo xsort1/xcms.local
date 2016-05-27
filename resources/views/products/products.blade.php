ee1833015c5cba79ad1860469606aa72744cc568
pokabudki <alexandr285@gmail.com>
2016-05-27 12:13:37 +0300

    Сделали модели мета, фото, категории дети

---
+++ resources/views/products/products.blade.php
@@ -0,0 +1,163 @@
+@extends('body')
+@section('centerbox')
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-lg-9">
+            <div class="text-center margin-b-50 margin-t-15">
+                <h1 class="page-title rowed">Клубы</h1>
+            </div>
+            
+            <div class="col-md-4 col-sm-6 club-wrapper">
+                    <a href="club.php" class="category">
+                        <img src="img/public/logo-decadance-club.png" alt="">
+                        <h3 class="main-title"><span>Decadance</span></h3>
+                        <div class="menu-divider-title"></div>
+                        <span class="menu-descripton">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum iste architecto, nam fugit quam vero eos, commodi eveniet deleniti ut iusto?</span>
+                        <div class="menu-button"><span>Посмотреть</span></div>
+                    </a>
+            </div>
+           <div class="col-md-4 col-sm-6 club-wrapper">
+                    <a href="club.php" class="category">
+                        <img src="img/public/logo-krysha-club.png" alt="">
+                        <h3 class="main-title"><span>Decadance</span></h3>
+                        <div class="menu-divider-title"></div>
+                        <span class="menu-descripton">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum iste architecto, nam fugit quam vero eos, commodi eveniet deleniti ut iusto?</span>
+                        <div class="menu-button"><span>Посмотреть</span></div>
+                    </a>
+            </div>
+            <div class="col-md-4 col-sm-6 club-wrapper">
+                    <a href="club.php" class="category">
+                        <img src="img/public/logo-art-club.png" alt="">
+                        <h3 class="main-title"><span>Decadance</span></h3>
+                        <div class="menu-divider-title"></div>
+                        <span class="menu-descripton">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum iste architecto, nam fugit quam vero eos, commodi eveniet deleniti ut iusto?</span>
+                        <div class="menu-button"><span>Посмотреть</span></div>
+                    </a>
+            </div>
+            <div class="col-md-4 col-sm-6 club-wrapper">
+                    <a href="club.php" class="category">
+                        <img src="img/public/logo-rust-club.jpg" alt="">
+                        <h3 class="main-title"><span>Decadance</span></h3>
+                        <div class="menu-divider-title"></div>
+                        <span class="menu-descripton">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum iste architecto, nam fugit quam vero eos, commodi eveniet deleniti ut iusto?</span>
+                        <div class="menu-button"><span>Посмотреть</span></div>
+                    </a>
+            </div>
+             <div class="col-md-4 col-sm-6 club-wrapper">
+                    <a href="club.php" class="category">
+                        <img src="img/public/logo-decadance-club.png" alt="">
+                        <h3 class="main-title"><span>Decadance</span></h3>
+                        <div class="menu-divider-title"></div>
+                        <span class="menu-descripton">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum iste architecto, nam fugit quam vero eos, commodi eveniet deleniti ut iusto?</span>
+                        <div class="menu-button"><span>Посмотреть</span></div>
+                    </a>
+            </div>
+           <div class="col-md-4 col-sm-6 club-wrapper">
+                    <a href="club.php" class="category">
+                        <img src="img/public/logo-krysha-club.png" alt="">
+                        <h3 class="main-title"><span>Decadance</span></h3>
+                        <div class="menu-divider-title"></div>
+                        <span class="menu-descripton">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum iste architecto, nam fugit quam vero eos, commodi eveniet deleniti ut iusto?</span>
+                        <div class="menu-button"><span>Посмотреть</span></div>
+                    </a>
+            </div>
+            <div class="col-md-4 col-sm-6 club-wrapper">
+                    <a href="club.php" class="category">
+                        <img src="img/public/logo-art-club.png" alt="">
+                        <h3 class="main-title"><span>Decadance</span></h3>
+                        <div class="menu-divider-title"></div>
+                        <span class="menu-descripton">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum iste architecto, nam fugit quam vero eos, commodi eveniet deleniti ut iusto?</span>
+                        <div class="menu-button"><span>Посмотреть</span></div>
+                    </a>
+            </div>
+            <div class="col-md-4 col-sm-6 club-wrapper">
+                    <a href="club.php" class="category">
+                        <img src="img/public/logo-rust-club.jpg" alt="">
+                        <h3 class="main-title"><span>Decadance</span></h3>
+                        <div class="menu-divider-title"></div>
+                        <span class="menu-descripton">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum iste architecto, nam fugit quam vero eos, commodi eveniet deleniti ut iusto?</span>
+                        <div class="menu-button"><span>Посмотреть</span></div>
+                    </a>
+            </div>
+             <div class="col-md-4 col-sm-6 club-wrapper">
+                    <a href="club.php" class="category">
+                        <img src="img/public/logo-decadance-club.png" alt="">
+                        <h3 class="main-title"><span>Decadance</span></h3>
+                        <div class="menu-divider-title"></div>
+                        <span class="menu-descripton">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum iste architecto, nam fugit quam vero eos, commodi eveniet deleniti ut iusto?</span>
+                        <div class="menu-button"><span>Посмотреть</span></div>
+                    </a>
+            </div>
+           <div class="col-md-4 col-sm-6 club-wrapper">
+                    <a href="club.php" class="category">
+                        <img src="img/public/logo-krysha-club.png" alt="">
+                        <h3 class="main-title"><span>Decadance</span></h3>
+                        <div class="menu-divider-title"></div>
+                        <span class="menu-descripton">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum iste architecto, nam fugit quam vero eos, commodi eveniet deleniti ut iusto?</span>
+                        <div class="menu-button"><span>Посмотреть</span></div>
+                    </a>
+            </div>
+            <div class="col-md-4 col-sm-6 club-wrapper">
+                    <a href="club.php" class="category">
+                        <img src="img/public/logo-art-club.png" alt="">
+                        <h3 class="main-title"><span>Decadance</span></h3>
+                        <div class="menu-divider-title"></div>
+                        <span class="menu-descripton">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum iste architecto, nam fugit quam vero eos, commodi eveniet deleniti ut iusto?</span>
+                        <div class="menu-button"><span>Посмотреть</span></div>
+                    </a>
+            </div>
+            <div class="col-md-4 col-sm-6 club-wrapper">
+                    <a href="club.php" class="category">
+                        <img src="img/public/logo-rust-club.jpg" alt="">
+                        <h3 class="main-title"><span>Decadance</span></h3>
+                        <div class="menu-divider-title"></div>
+                        <span class="menu-descripton">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum iste architecto, nam fugit quam vero eos, commodi eveniet deleniti ut iusto?</span>
+                        <div class="menu-button"><span>Посмотреть</span></div>
+                    </a>
+            </div>
+             <div class="col-md-4 col-sm-6 club-wrapper">
+                    <a href="club.php" class="category">
+                        <img src="img/public/logo-decadance-club.png" alt="">
+                        <h3 class="main-title"><span>Decadance</span></h3>
+                        <div class="menu-divider-title"></div>
+                        <span class="menu-descripton">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum iste architecto, nam fugit quam vero eos, commodi eveniet deleniti ut iusto?</span>
+                        <div class="menu-button"><span>Посмотреть</span></div>
+                    </a>
+            </div>
+           <div class="col-md-4 col-sm-6 club-wrapper">
+                    <a href="club.php" class="category">
+                        <img src="img/public/logo-krysha-club.png" alt="">
+                        <h3 class="main-title"><span>Decadance</span></h3>
+                        <div class="menu-divider-title"></div>
+                        <span class="menu-descripton">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum iste architecto, nam fugit quam vero eos, commodi eveniet deleniti ut iusto?</span>
+                        <div class="menu-button"><span>Посмотреть</span></div>
+                    </a>
+            </div>
+            <div class="col-md-4 col-sm-6 club-wrapper ">
+                    <a href="club.php" class="category">
+                        <img src="img/public/logo-art-club.png" alt="">
+                        <h3 class="main-title"><span>Decadance</span></h3>
+                        <div class="menu-divider-title"></div>
+                        <span class="menu-descripton">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum iste architecto, nam fugit quam vero eos, commodi eveniet deleniti ut iusto?</span>
+                        <div class="menu-button"><span>Посмотреть</span></div>
+                    </a>
+            </div>
+            
+            <div class="club-items">
+                <a href="layout/pages/club-page-2.php">Следущая страница</a>
+            </div>
+           <div class="col-lg-12 margin-t-30 club-pagin">
+                <?php /* include 'layout/pagination.php'; */ ?>
+            </div>
+        </div>
+
+        <div class="col-md-3 visible-lg hidden-xs hidden-sm ">
+            <div onclick="location.href='http://google.ru';" class="banner banner-right first" style="background:url(img/public/first-pub.png)"></div>
+
+            <div onclick="location.href='http://google.ru';" class="banner banner-right second" style="background:url(img/public/second-pub.png)"></div>
+
+        </div>
+         
+    </div>
+</div>
+@stop
